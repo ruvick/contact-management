5643186d5f24b8ebdcadaf1e9c36d57648ff3071
Ruvick <rudikov.web@ya.ru>
2024-11-14 23:07:02 +0300

    Компонент ContactForm

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,60 +1,101 @@
+<script setup>
+	import { ref, watch, defineProps, defineEmits } from 'vue';
+
+	const props = defineProps({
+		currentContact: {
+			type: Object,
+			default: null
+		}
+	});
+
+	const emit = defineEmits(['addContact', 'editContact']);
+
+	const name = ref('');
+	const phone = ref('');
+	const email = ref(''); // Добавлено поле для email
+	const isEditing = ref(false);
+
+	watch(() => props.currentContact, (newValue) => {
+		if (newValue) {
+			name.value = newValue.name;
+			phone.value = newValue.phone;
+			email.value = newValue.email; // Заполнение поля email при редактировании
+			isEditing.value = true;
+		} else {
+			resetForm();
+		}
+	});
+
+	const submitForm = () => {
+		const contactData = { name: name.value, phone: phone.value, email: email.value }; // Добавлено поле email
+		
+		if (isEditing.value) {
+			emit('editContact', contactData);
+		} else {
+			emit('addContact', contactData);
+		}
+		
+		resetForm();
+	};
+
+	const resetForm = () => {
+		name.value = '';
+		phone.value = '';
+		email.value = ''; // Сброс поля email
+		isEditing.value = false;
+	};
+</script>
+
 <template>
-	<form @submit.prevent="submitForm">
-	  <input v-model="name" placeholder="Имя" required />
-	  <input v-model="phone" placeholder="Телефон" required />
-	  <input v-model="email" type="email" placeholder="Email" required />
-	  <button type="submit">{{ isEditing ? 'Обновить' : 'Добавить' }}</button>
+
+	<form @submit.prevent="submitForm" class="control__add add-control">
+		<div class="add-control__title">Добавить контакт</div>
+		<div class="add-control__inner">
+
+			<div class="add-control__field field">
+				<input id="input" class="field__input" name="form[]" autocomplete="off" type="name" placeholder=" " 
+					data-error="Заполните поле" data-required="name" data-validate v-model="name" required>
+				<label for="input" class="field__label">Имя <span>*</span></label>
+			</div>
+
+			<div class="add-control__field field">
+				<input id="input" class="field__input" name="tel" autocomplete="off" type="number" placeholder=" " 
+				data-error="Заполните поле" data-required="tel" data-validate v-model="phone" required>
+				<label for="input" class="field__label">Телефон <span>*</span></label>
+			</div>
+
+			<div class="add-control__field field">
+				<input id="input" class="field__input" name="email" autocomplete="off" placeholder=" " 
+				data-error="Неверный Email" data-required="email" data-validate v-model="email" type="email" required>
+				<label for="input" class="field__label">Email <span>*</span></label>
+			</div>
+
+			<button type="submit" class="add-control__btn btn">{{ isEditing ? 'Обновить' : 'Добавить' }}</button>
+
+		</div>
 	</form>
+
  </template>
  
- <script setup>
- import { ref, watch, defineProps, defineEmits } from 'vue';
- 
- const props = defineProps({
-	currentContact: {
-	  type: Object,
-	  default: null
+ <style scoped lang="scss">
+		.add-control {
+			&__title {
+				font-size: 1.4375rem;
+				font-weight: 500;
+				&:not(:last-child){
+					margin-bottom: 0.9375rem;
+				}
+			}
+			&__inner {
+			}
+			&__field {
+				&:not(:last-child){
+					margin-bottom: 0.625rem;
+				}
+			}
+			&__btn {
+				display: flex;
+				margin-left: auto;
+			}
 	}
- });
- 
- const emit = defineEmits(['addContact', 'editContact']);
- 
- const name = ref('');
- const phone = ref('');
- const email = ref(''); // Добавлено поле для email
- const isEditing = ref(false);
- 
- watch(() => props.currentContact, (newValue) => {
-	if (newValue) {
-	  name.value = newValue.name;
-	  phone.value = newValue.phone;
-	  email.value = newValue.email; // Заполнение поля email при редактировании
-	  isEditing.value = true;
-	} else {
-	  resetForm();
-	}
- });
- 
- const submitForm = () => {
-	const contactData = { name: name.value, phone: phone.value, email: email.value }; // Добавлено поле email
-	
-	if (isEditing.value) {
-	  emit('editContact', contactData);
-	} else {
-	  emit('addContact', contactData);
-	}
-	
-	resetForm();
- };
- 
- const resetForm = () => {
-	name.value = '';
-	phone.value = '';
-	email.value = ''; // Сброс поля email
-	isEditing.value = false;
- };
- </script>
- 
- <style scoped>
- /* Стили для формы */
  </style>
