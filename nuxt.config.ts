ff7526577aa36c72b1adb372c22d9a1c16cb5b98
Ruvick <rudikov.web@ya.ru>
2024-11-15 12:54:59 +0300

    Обнов

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
-	devtools: { enabled: true },
-	css: [
-		'@/assets/styles/main.scss',
-		'@/assets/styles/null.scss',
-	]
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: [
+    '@/assets/styles/main.scss',
+    '@/assets/styles/null.scss',
+  ],
+  target: 'static',
 });
+
